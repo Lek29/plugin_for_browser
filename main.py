419959c37904a2a9b2a00312d769f61d0b54cb98
Lek <ligioner29@mail.ru>
2026-02-13 06:17:11 +0800

    added article raiting calculation

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import pymorphy2
 import aiohttp
 import asyncio
+
+from text_tools import split_by_words, calculate_jaundice_rate
+from adapters.inosmi_ru import sanitize
 
 
 async def fetch(session, url):
@@ -9,9 +13,23 @@ async def fetch(session, url):
 
 
 async def main():
+    url = 'https://inosmi.ru/20260212/olimpiada-277075823.html'
+    test_charged_words = ['политический', 'украина', 'лозунги']
+
+    morph = pymorphy2.MorphAnalyzer()
+
     async with aiohttp.ClientSession() as session:
-        html = await fetch(session, 'http://example.com')
-        print(html)
+        async with session.get(url) as response:
+            response.raise_for_status()
+            html = await response.text()
 
+    clean_text = sanitize(html, plaintext=True)
 
-asyncio.run(main())
+    article_words = split_by_words(morph, clean_text)
+    rate = calculate_jaundice_rate(article_words, test_charged_words)
+
+    print(f'Рейтинг: {rate}')
+    print(f'Слов в статье: {len(article_words)}')
+
+if __name__ == '__main__':
+    asyncio.run(main())
