7d70c3c82178dc77709150a66208458236cf9586
Lek <ligioner29@mail.ru>
2026-02-17 14:22:59 +0800

    integrated text analysis into web server

--- server.py
+++ server.py
@@ -1,7 +1,15 @@
+import aiohttp
+import anyio
 from aiohttp import web
+import asyncio
+import pymorphy2
+from functools import partial
+
+from main import processing_article, ProcessingStatus
+from text_tools import load_charged_words
 
 
-async def handle_analyse(request):
+async def handle_analyse(morph, charged_words, request):
     urls_raw = request.query.get('urls')
     if not urls_raw:
         return web.json_response(
@@ -10,15 +18,40 @@ async def handle_analyse(request):
         )
     urls = urls_raw.split(',')
 
-    return web.json_response({
-        'urls': urls,
-    })
+    if len(urls) > 10:
+        return web.json_response({'error': 'too many urls'}, status=200)
+
+    results = []
+
+    async with aiohttp.ClientSession() as session:
+        async with anyio.create_task_group() as tg:
+            for url in urls:
+                tg.start_soon(
+                    processing_article,
+                    session,
+                    morph,
+                    url,
+                    charged_words,
+                    results
+                )
+
+    return web.json_response(results)
+
+
+def make_app(morph_analyzer, words_dictonary):
+    app_instance = web.Application()
+
+    handler = partial(handle_analyse, morph_analyzer, words_dictonary)
+
+    app.add_routes([web.get('/analyse', handler)])
+
+    return app_instance
+
 
 if __name__ == '__main__':
-    app = web.Application()
+    morph = pymorphy2.MorphAnalyzer()
+    charged_words = load_charged_words('charged_dict')
 
-    app.add_routes([
-        web.get('/', handle_analyse),
-    ])
+    app = make_app(morph, charged_words)
 
     web.run_app(app, port=8080)
